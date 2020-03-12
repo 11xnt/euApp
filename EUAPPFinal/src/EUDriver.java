6dc370df773677d30cf5a77d113de126838d5682
allen <allenterescencobq@gmail.com>
2020-03-12 14:19:43 +0000

    Fixed some methods -- Still very buggy!

--- EUAPPFinal/src/EUDriver.java
+++ EUAPPFinal/src/EUDriver.java
@@ -414,7 +414,7 @@ public class EUDriver {
             String listMEPsBySpecificParty = "";
             while (getPartyList() > mep)
             {
-                if(partyList.getPartyList(i).get) {
+                if(partyList.getPartyList(i).ge) {
                     listMEPsBySpecificParty = listMEPsBySpecificParty + i + ": " + getParty(i) + "\n";
                 }
 
